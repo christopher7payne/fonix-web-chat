526b7d60225e26b6eb0a71a3b914e1f69626ef28
Chris Payne <chris@boilerjuice.com>
2022-05-28 12:37:18 +0100

    add messages model that is referenced to the rooms and the user

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   validates_uniqueness_of :name
+  has_many :messages
 
   scope :open_rooms, -> { where(enabled: true) }
   scope :closed_rooms, -> { where(enabled: false) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates_uniqueness_of :email
+
+  has_many :messages
 end
